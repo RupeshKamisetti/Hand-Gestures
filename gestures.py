b35b0a86c63215f66566ef7b84cb415e6125260c
Rupesh Kamisetti <rupeshkamisetti@gmail.com>
2024-07-28 16:56:23 +0530

    Add files via upload

---
+++ gestures.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = cv2.flip(frame, 1)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            landmark_list = []
+            for id, lm in enumerate(hand_landmarks.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                landmark_list.append([cx, cy])
+            if len(landmark_list) >= 9:
+                gesture = None
+                if (landmark_list[8][1] < landmark_list[6][1] and 
+                    landmark_list[12][1] < landmark_list[10][1] and 
+                    landmark_list[16][1] < landmark_list[14][1] and
+                    landmark_list[20][1] < landmark_list[18][1] and 
+                    landmark_list[4][1] < landmark_list[3][1]
+                ):
+                    gesture = "HEY! I'm Rupesh"
+                if (landmark_list[4][1] > landmark_list[3][1] and 
+                    landmark_list[8][1] < landmark_list[6][1]
+                ):
+                    gesture = "I'm an Artist"
+                if gesture:
+                    text_x = landmark_list[0][0]
+                    text_y = landmark_list[0][1] - 30
+                    cv2.putText(frame, gesture, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                 
+    cv2.imshow('Hand Gesture Recognition', frame)
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
